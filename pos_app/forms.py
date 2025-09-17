43c624e6c605b68b90c728ea2f71a4bf4bb711e3
LAB23_USER ON WS27 <L23X16W27@LabHCI.net>
2025-09-17 17:19:53 +0800

    feature(forms): create_forms

--- pos_app/forms.py
+++ pos_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile, Product, Transaction, TransactionItem
+from .models import Profile, Product, Transaction, TransactionItem # type: ignore
 
 
 class UserRegisterForm(UserCreationForm):
